49958e891e2a40b50a8a535eae8e25bb14924d62
Dafne Salas <dafnesalvatore1903@gmail.com>
2024-01-23 17:57:11 -0300

    operadores de atribuicao

--- alura_4.py
+++ alura_4.py
@@ -5,6 +5,24 @@ while n <= 10:
     print(n)
     n = n + 1
 '''''
+ alunos = [10, 15, 20, 25, 30]
+# index = 0
+# while index < len(alunos):
+#     print(alunos[index])
+#     index = index + 1
+
+salarios = [33, 21, 30, 45]
+soma_salarios = 0.0
+index = 0
+while index < len(salarios):
+    print(salarios)
+    print('index', index)
+    print('valor', salarios[index])
+    soma_salarios += salarios[index]
+    print('soma', soma_salarios)
+    index += 1
+    print('-----------------------')
+    
 #03 médias de aluno 
 '''''
 contador = 1
@@ -15,6 +33,7 @@ while contador <=3:
     contador += 1
     '''''
 
+#operadores de atribuição
 preco = 2.00
 preco += 3
 print(preco)
